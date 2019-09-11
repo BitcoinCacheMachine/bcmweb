bb910d005ae896605e49ec435739eb70a3a81319
derek <derek@farscapian.com>
2019-09-10 20:15:32 -0500

    Updates to clear tificate issue.

--- _site/docker.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export JEKYLL_VERSION=3.8
-docker run -it --rm --volume="$PWD:/srv/jekyll" jekyll/jekyll:$JEKYLL_VERSION jekyll build
--- docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export JEKYLL_VERSION=3.8
-
-docker pull jekyll/jekyll:$JEKYLL_VERSION
-
-docker run -it --rm --volume="$PWD:/srv/jekyll" jekyll/jekyll:$JEKYLL_VERSION jekyll build
